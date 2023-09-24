0f4da068f87cdf15585eea8aae681cefbeb614fc
matpakla <matpakla@gmail.com>
2023-09-24 15:54:46 +0200

    finish

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-function aktualizuj() {
-    let nowa = prompt("Podaj nową wartość: ")
+function aktualizuj(element) {
+    element.innerText = prompt("Podaj nową ilość:")
+    zaznacz()
 }
 
-function zamow() {
-    
+let id_zamowienia = 0
+
+function zamow(element) {
+    id_zamowienia += 1
+    alert(`Zamówienie nr: ${id_zamowienia} Produkt: ${element.innerText}`)
 }
 
 function zaznacz() {
-    let ilosc1 = document.querySelector("#ilosc1")
-    let ilosc2 = document.querySelector("#ilosc2")
-    let ilosc3 = document.querySelector("#ilosc3")
-    let ilosc4 = document.querySelector("#ilosc4")
-    
-    if (ilosc1.innerText == 0) ilosc1.style.background = "red"
-    else if (ilosc1.innerText >= 1 && ilosc1.innerText <=5) ilosc1.style.background = "yellow"
-    else ilosc1.style.background = "honeydew"
-    
-    if (ilosc2.innerText == 0) ilosc2.style.background = "red"
-    else if (ilosc2.innerText >= 1 && ilosc2.innerText <=5) ilosc2.style.background = "yellow"
-    else ilosc2.style.background = "honeydew"
-    
-    if (ilosc3.innerText == 0) ilosc3.style.background = "red"
-    else if (ilosc3.innerText >= 1 && ilosc3.innerText <=5) ilosc3.style.background = "yellow"
-    else ilosc3.style.background = "honeydew"
-    
-    if (ilosc4.innerText == 0) ilosc4.style.background = "red"
-    else if (ilosc4.innerText >= 1 && ilosc4.innerText <=5) ilosc4.style.background = "yellow"
-    else ilosc1.style.background = "honeydew"
+    for (let i = 1; i < 5; i++) {
+        let tag = "#ilosc" + i
+        let element = document.querySelector(tag)
+
+        if (element.innerText == 0) element.style.background = "red"
+        else if (element.innerText >= 1 && element.innerText <= 5) element.style.background = "yellow"
+        else element.style.background = "honeydew"
+    }
 }
 
 zaznacz(0)
